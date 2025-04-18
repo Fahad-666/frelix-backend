c81839cdd9d371012261242248d8b653f3f6a60b
Fahad-666 <itfahad102@gmail.com>
2025-04-18 20:02:13 +0500

    FIXESSSSSSS

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from config.database import Base, engine
@@ -17,4 +18,5 @@ def login():
     return login_user()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
