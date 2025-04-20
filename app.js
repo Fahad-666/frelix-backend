575442978b7278997411137c4da3466d94d9e412
Fahad-666 <itfahad102@gmail.com>
2025-04-20 21:41:16 +0500

    Configure CORS options for improved security and functionality

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const cors = require('cors');
 const { registerUser, loginUser } = require('./controllers/userController');
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
 app.post('/register', registerUser);
