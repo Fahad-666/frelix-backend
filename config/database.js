33f0d8db12143c6e6809b28c53936bed331e3fe7
Fahad-666 <itfahad102@gmail.com>
2025-04-22 21:30:09 +0500

    Update database configuration with correct username and password in database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const DB_USERNAME = 'root';
-const DB_HOST = '127.0.0.1';
+const DB_USERNAME = 'superb-dream-457615-r0:us-central1:frelix';
+const DB_HOST = '104.154.245.120';
 const DB_PORT = '3306';
 const DB_NAME = 'frelix';
-const DB_PASSWORD = '';
+const DB_PASSWORD = 'Fahad@2008';
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
