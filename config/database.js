293efc3bd2e12a4b0fc8b5d4cfb4109cfb536b49
Fahad-666 <itfahad102@gmail.com>
2025-04-22 20:15:50 +0500

    Remove environment variable fallback for database configuration in database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const DB_USERNAME = process.env.DB_USERNAME || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'Fahad@2008';
-const DB_HOST = process.env.DB_HOST || '127.0.0.1';
-const DB_PORT = process.env.DB_PORT || 3306;
-const DB_NAME = process.env.DB_NAME || 'frelix';
+const DB_USERNAME = 'root';
+const DB_HOST = '127.0.0.1';
+const DB_PORT = '3306';
+const DB_NAME = 'frelix';
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
