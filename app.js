2ef14fb8f053e27d750869d7ef2b6e57e2c46523
Fahad-666 <itfahad102@gmail.com>
2025-04-23 16:04:12 +0500

    Update server listen address to allow external connections

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/verify-token', verifyToken);
 app.get('/get-profile', getUserProfile);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   sequelize.authenticate()
   .then(() => console.log('Database connected!'))
